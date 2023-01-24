1e21418ce8d2892a56a42f377c7d87540aa71780
Adelain <adelaindalmeijer@gmail.com>
2023-01-24 15:17:25 +0100

    add Assignment 2

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 // function for creating the dynamic table
 function createTable() {
     // getting the table data from the database
-    $.ajax({
-        url: "https://wt.ops.labs.vu.nl/api23/12b12061",
-        method: 'get',
-        dataType: 'json',
-        success: function(data) {
+    fetch("https://wt.ops.labs.vu.nl/api23/12b12061", {
+            method:'GET',
+            headers: {
+                'Content-type': 'application/json',
+            },
+          })
+        .then((response) => response.json())
+        .then((data) => {
             // emptying the current table so that it doesn't duplicate
             $("#body").empty();
             // looping through the data array
             for(var i = 0; i < data.length; i++) {
                 // appending the table contents in html
-                $("#body").append("<tr>" + '\n' + "<td onclick=\"createTable()\">" + data[i].author + "</td>" + '\n' +
+                $("#body").append("<tr>" + '\n' + "<td>" + data[i].author + "</td>" + '\n' +
                                     "<td>" + data[i].alt + "</td>" + '\n' +
                                     "<td>" + data[i].tags + "</td>" + '\n' +
                                     "<td>" + data[i].description + "</td>" + '\n' +
                                     "<td>" + "<img src=\"" + data[i].image + "\"" + "alt= \"Image of " + data[i].product + "\" width = \"200px\" height = \"200px\">" + "</td>" + "</tr>"
-                                    );
+                                );
             }
-        }
-    });
+        });
 }
 
 // reset button
 $("#reset").click(function() {
-    $.get("https://wt.ops.labs.vu.nl/api23/12b12061/reset", "json");
+    fetch("https://wt.ops.labs.vu.nl/api23/12b12061/reset");
 
     createTable();
 });
@@ -39,7 +41,14 @@ $("#poster").click(function() {
         image: $("#image").val()
     };
     let postData = data;
-    $.post("https://wt.ops.labs.vu.nl/api23/12b12061", postData, "json");
+    fetch("https://wt.ops.labs.vu.nl/api23/12b12061", {
+        method: 'POST',
+        headers: {
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json());
 
     createTable();
     modal.style.display = "none";
